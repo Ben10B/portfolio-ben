a9cad27f4f27669fa8ebc0c0a72d162de0cf5cdb
Benjamin Beckwith <benjamin.beckwith@yahoo.com>
2021-08-11 12:29:24 -0600

    updated data

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,6 @@
 //web
 import hub from '../assets/HUB2.gif';
+import nad1 from '../assets/NAD1.gif';
 import pizzaProject from '../assets/Slice Pizzeria.png';
 import innovationProject from '../assets/InnovationLab.png';
 import mazeProject from '../assets/AmazING.png';
@@ -31,15 +32,21 @@ const tjIntro = 'https://youtube.com/watch?v=7g-U5sHU4h0';
 
 export const mainData = [
   {
-    index: 0, image: hub, title: 'Kolvanta Order Management System', 
+    index: 0, image: nad1, title: 'NewAge Diamonds', 
+    description: 'Migrated React website to Shopify platform and redesigned theme to include custom features for Ariix (now renamed as NewAge).'
+    + ' As a result, traffic and sales increased.', 
+    tech: ['Shopify Liquid', 'JavaScript', 'Express', 'Node']
+  },
+  {
+    index: 1, image: hub, title: 'Kolvanta Order Management System', 
     description: 'Created an order management system to keep track of purchased orders from processed to delivered;'
-    + ' moving away from Google Sheets. (This system also kept track of orders for Do Good Be Kind for one of their promotions.)', 
+    + " moving away from Google Sheets. Kept track of orders for Ariix, and one of Do Good Be Kind's promotions.", 
     tech: ['MongoDB', 'Express', 'React', 'Node']
   },
-  { index: 1, video: news, title: `XYZ Breaking News`, 
+  { index: 2, video: news, title: `XYZ Breaking News`, 
     description: '', tech: ['Premiere Pro'] 
   },
-  { index: 2, video: seven, title: `7 into 28`, 
+  { index: 3, video: seven, title: `7 into 28`, 
     description: 'Short Typography', tech: ['After Effects'] 
   },
 ];
