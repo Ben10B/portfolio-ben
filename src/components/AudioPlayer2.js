0fb901ed4d8adfb2bde37b54e1c20f4b5bd0fb4b
Benjamin Beckwith <benjamin.beckwith@yahoo.com>
2021-09-04 23:26:14 -0600

    Updated audio component
    Added close button.
    Updated CSS

--- src/components/AudioPlayer2.js
+++ src/components/AudioPlayer2.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { connect } from 'react-redux';
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
+import '../css/audio.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { updateAudio } from '../actions/settingsActions';
 
-const AudioPlayer2 = ({ settings }) => {
+const AudioPlayer2 = ({ settings, updateAudio }) => {
   return (
     <div className={`audio-player-wrapper ${settings.audio.control}`}>
-      <AudioPlayer
-      src={require(`../assets/audio/${settings.audio.song}.mp3`).default}
-      header={`Now Playing: ${settings.audio.song}`}
-      showJumpControls={false}
-      className="audio-player"
-      autoPlayAfterSrcChange
-      layout="stacked-reverse"
-    />
+      <div className="audio-player-content">
+        <div className="close" onClick={() => updateAudio({ control: 'hide' })}>
+          <FontAwesomeIcon icon={'times'}/>
+        </div>
+        <AudioPlayer
+        src={require(`../assets/audio/${settings.audio.song}.mp3`).default}
+        header={`Now Playing: ${settings.audio.song}`}
+        showJumpControls={false}
+        className="audio-player"
+        autoPlayAfterSrcChange
+        layout="stacked-reverse"
+        />
+      </div>
     </div>
   )
 }
@@ -23,4 +31,9 @@ const mapStateToProps = (state) => {
     settings: state.settings
   }
 }
-export default connect(mapStateToProps, {})(AudioPlayer2);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    updateAudio: (ownProps) => dispatch(updateAudio(ownProps)),
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(AudioPlayer2);
