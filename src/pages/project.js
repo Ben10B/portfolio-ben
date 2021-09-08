7e668d2d737c9fd62d96285658b6087042ecf475
Benjamin Beckwith <benjamin.beckwith@yahoo.com>
2021-09-07 22:38:34 -0600

    Updated project page

--- src/pages/project.js
+++ src/pages/project.js
@@ -5,6 +5,8 @@ import Header from '../components/Header';
 import Loader from '../components/Loader';
 import VideoPlayer from '../components/VideoPlayer';
 import '../css/project.css';
+import ytube from '../assets/YouTube-icon-3-fff.png';
+import web from '../assets/Spiderweb.png';
 
 class Project extends Component {
   componentDidMount() {
@@ -23,7 +25,16 @@ class Project extends Component {
           <h6 className="item-b">{project && project.title}</h6>
           <div className="item-c">{project && project.description}</div>
           <div className="item-d"><h6>SKILLS:</h6> <div className="row">{project && project.tech.map(x => <p key={x}>{x}</p>)}</div></div>
-          <div className="item-e"></div>
+          <div className="row item-e">
+            {
+              (project && project.links) && project.links.map((x, index) => (
+                <div key={index} className="socialIcon">
+                  <div style={{backgroundImage: `url(${x.yt ? ytube : web})`}}></div>
+                  <a href={x.yt ? x.yt : (x.direct ? x.direct : null)} target="_blank" rel="noopener noreferrer"> </a>
+                </div>
+              ))
+            }
+          </div>
         </div>
       </div>
     )
